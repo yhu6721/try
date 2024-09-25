dbfcbbcc78e56066a435c2cdbfaabbd703863ab2
yhu6721 <yhu6721@gmail.com>
2024-09-25 23:32:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function goToSection(sectionId) {
-    // 隐藏所有 section
-    document.querySelectorAll('section').forEach(section => {
-        section.classList.add('hidden');
-    });
 
-    // 显示指定的 section
-    document.getElementById(sectionId).classList.remove('hidden');
-}
